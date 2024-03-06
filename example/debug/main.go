9488dd6f1505da30ba70c5e54303de6780886517
Alan Xu <xdpcsyy@gmail.com>
2024-03-06 14:03:15 +0800

    docs(examples): update examples (#41)
    * docs(examples): update examples
    
    update examples

---
+++ example/debug/main.go
@@ -0,0 +1,26 @@
+package main
+
+// @Title        main.go
+// @Description
+// @Create       XdpCs 2023-10-12 12:44
+// @Update       XdpCs 2023-10-20 20:25
+
+import (
+	"context"
+
+	"github.com/XdpCs/wujiesdk"
+)
+
+func main() {
+	c, err := wujiesdk.NewCredentials("appID", "PrivateKey")
+	if err != nil {
+		panic(err)
+	}
+
+	client := wujiesdk.NewDebugClient(c)
+	ca := wujiesdk.NewCaller(client)
+	_, _, err = ca.CancelImage(context.Background(), "2087C400944DF2D6B25BED29C910B1B8")
+	if err != nil {
+		panic(err)
+	}
+}
